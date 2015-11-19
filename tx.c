d71aa63eafaa3954993c93bde061e9b3a7f31051
brb7nu <brb7nu@virginia.edu>
2015-11-19 09:24:07 -0500

    add base case to NormalXmit state

--- tx.c
+++ tx.c
@@ -61,6 +61,8 @@ void Xmit(TransmitterData* TData) {
 			break;
 		}
 
+        if (TData->Bits_Remaining == 0) TData->Transmitter_State = InterWord;
+
 		break;
 	case InterWord:
 		switch (Phase) {
